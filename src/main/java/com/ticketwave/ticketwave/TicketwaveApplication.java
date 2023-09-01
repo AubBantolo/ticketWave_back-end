c3ce1f1fb6767e9b84798a23ec3168e0d574f71d
AubBantolo <jhuliet14@gmail.com>
2023-09-01 20:02:55 +0800

    feat: new project

---
+++ src/main/java/com/ticketwave/ticketwave/TicketwaveApplication.java
@@ -0,0 +1,13 @@
+package com.ticketwave.ticketwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketwaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketwave/ticketwave/TicketwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketwave.ticketwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
